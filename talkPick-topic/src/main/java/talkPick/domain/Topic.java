f6014a01e06ba8bbb2499c62d76335b5db8892f3
M-ung <wjdahrrla@gmail.com>
2025-02-13 17:21:32 +0900

    Feat: Topic 도메인 구현해라 - #5

---
+++ talkPick-topic/src/main/java/talkPick/domain/Topic.java
@@ -0,0 +1,19 @@
+package talkPick.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import talkPick.model.BaseTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Topic extends BaseTime {
+    private Long id;
+    private String content;
+    private long averageTalkTime;
+    private int selectCount;
+    private int likeCount;
+}
