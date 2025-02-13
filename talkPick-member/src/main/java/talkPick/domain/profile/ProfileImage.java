2d3f8046b97b0c2fc059e118f4173185539a53e5
M-ung <wjdahrrla@gmail.com>
2025-02-13 17:02:20 +0900

    Feat: Profile 이미지 도메인 구현해라 - #5

--- talkPick-member/src/main/java/talkPick/domain/profile/ProfileImage.java
+++ talkPick-member/src/main/java/talkPick/domain/profile/ProfileImage.java
@@ -1,4 +1,18 @@
 package talkPick.domain.profile;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import talkPick.model.TalkPickStatus;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfileImage {
+    private Long id;
+    private Long profileId;
+    private String url;
+    private TalkPickStatus status;
 }
