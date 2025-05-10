5a8473d9a1adefed7719de23d89552386ab2180d
M-ung <wjdahrrla@gmail.com>
2025-05-10 22:20:13 +0900

    Feat: Test 코드 추가해라 - #58

--- src/test/java/talkPick/talkPick/TalkPickApplicationTests.java
+++ src/test/java/talkPick/talkPick/TalkPickApplicationTests.java
@@ -1,10 +1,10 @@
-package jquery.jquery;
+package talkPick.talkPick;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JqueryApplicationTests {
+class TalkPickApplicationTests {
 
     @Test
     void contextLoads() {
