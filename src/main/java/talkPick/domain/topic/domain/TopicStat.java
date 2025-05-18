f99f278c685f0f39407fdf82945dbb48af14f653
M-ung <wjdahrrla@gmail.com>
2025-05-18 17:35:34 +0900

    Feat: TopicStat에 락 보완을 위해 Version 어노테이션 추가 및 update 메서드 추가해라 - #86

--- src/main/java/talkPick/domain/topic/domain/TopicStat.java
+++ src/main/java/talkPick/domain/topic/domain/TopicStat.java
@@ -1,10 +1,13 @@
 package talkPick.domain.topic.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import talkPick.domain.member.domain.Member;
+import talkPick.domain.member.domain.profile.Profile;
+import talkPick.domain.member.domain.type.Gender;
+import talkPick.domain.member.domain.type.MBTI;
+
+import java.time.LocalDate;
 
 //TODO 동시성 고려해야 함.
 @Getter
@@ -15,7 +18,7 @@ import lombok.*;
 public class TopicStat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; //TODO 토픽 ID가 갈아치울 예정
+    private Long id;
     private Long topicId;
     private Integer eCount;
     private Integer iCount;
@@ -33,6 +36,11 @@ public class TopicStat {
     private Integer thirtiesCount;
     private Integer fortiesCount;
     private Integer fiftiesCount;
+    private Integer maleCount;
+    private Integer femaleCount;
+
+    @Version
+    private Long version;
 
     public static TopicStat of(Long topicId) {
         return TopicStat.builder()
@@ -53,10 +61,83 @@ public class TopicStat {
                 .thirtiesCount(0)
                 .fortiesCount(0)
                 .fiftiesCount(0)
+                .maleCount(0)
+                .femaleCount(0)
                 .build();
     }
 
     public void addLike() {
         this.likeCount++;
+    }
+
+    public void update(Member member, Profile profile, long averageTalkTime) {
+        MBTI mbti = MBTI.INFP;
+        updateMBTI(mbti);
+        updateAge(member.getBirth());
+        updateGender(member.getGender());
+        updateAverageTalkTime(talkTime);
+        this.selectCount++;
+    }
+
+    private void updateMBTI(MBTI mbti) {
+        if (mbti != null) {
+            String mbtiString = mbti.name();
+            if (mbtiString.startsWith("E")) {
+                this.eCount++;
+            } else if (mbtiString.startsWith("I")) {
+                this.iCount++;
+            }
+            if (mbtiString.charAt(1) == 'S') {
+                this.sCount++;
+            } else if (mbtiString.charAt(1) == 'N') {
+                this.nCount++;
+            }
+            if (mbtiString.charAt(2) == 'F') {
+                this.fCount++;
+            } else if (mbtiString.charAt(2) == 'T') {
+                this.tCount++;
+            }
+            if (mbtiString.charAt(3) == 'J') {
+                this.jCount++;
+            } else if (mbtiString.charAt(3) == 'P') {
+                this.pCount++;
+            }
+        }
+    }
+
+    private void updateAge(LocalDate birth) {
+        if (birth != null) {
+            int age = LocalDate.now().getYear() - birth.getYear();
+
+            if (age < 20) {
+                this.teenCount++;
+            } else if (age < 30) {
+                this.twentiesCount++;
+            } else if (age < 40) {
+                this.thirtiesCount++;
+            } else if (age < 50) {
+                this.fortiesCount++;
+            } else {
+                this.fiftiesCount++;
+            }
+        }
+    }
+
+    private void updateGender(Gender gender) {
+        if (gender != null) {
+            if (gender == Gender.MALE) {
+                this.maleCount++;
+            } else if (gender == Gender.FEMALE) {
+                this.femaleCount++;
+            }
+        }
+    }
+
+    private void updateAverageTalkTime(long talkTime) {
+        if (this.selectCount == 1) {
+            this.averageTalkTime = talkTime;
+        } else {
+            this.averageTalkTime = ((this.averageTalkTime * (this.selectCount - 1)) + talkTime) / this.selectCount;
+        }
     }
 }
