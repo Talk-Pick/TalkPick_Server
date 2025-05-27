ebbea7a60b04022a0f39bb36f8fd67c930163f5c
M-ung <wjdahrrla@gmail.com>
2025-05-27 16:47:11 +0900

    Style: 검색 도메인 변수명 keyword -> word로 변경해라 - #96

--- src/main/java/talkPick/domain/search/adapter/out/TopicSearchHistoryCommandRepositoryAdapter.java
+++ src/main/java/talkPick/domain/search/adapter/out/TopicSearchHistoryCommandRepositoryAdapter.java
@@ -20,13 +20,13 @@ public class TopicSearchHistoryCommandRepositoryAdapter implements TopicSearchHi
     @Async
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void save(Long memberId, String keyword, Boolean isResultShown) {
+    public void save(Long memberId, String word, Boolean isResultShown) {
         //TODO 테스트할 때 할 일 : 트랜잭션/스레드 다른지 확인
         String threadName = Thread.currentThread().getName();
         boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
         Object txResource = TransactionSynchronizationManager.getResource("javax.persistence.EntityManager");
         log.info("[Save] thread = {}, tx active = {}, tx resource = {}", threadName, isActive, txResource);
 
-        topicSearchHistoryJpaRepository.save(TopicSearchHistory.of(memberId, keyword, isResultShown));
+        topicSearchHistoryJpaRepository.save(TopicSearchHistory.of(memberId, word, isResultShown));
     }
 }
