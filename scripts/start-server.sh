b7cd799f694181711fc3ffcd90876c1784f9efa1
Hszoo <trre1827151@gmail.com>
2025-07-19 16:26:58 +0900

    [Deploy] 브랜치 이름 변경

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,22 +3,12 @@ set -e
 
 echo "--------------- START : Talkpick Server Deploy -----------------"
 
-CONTAINER_NAME=talkpick-server
-IMAGE_URI=718513646976.dkr.ecr.ap-northeast-2.amazonaws.com/talkpick-server/talkpick-server:latest
+  cd /home/ubuntu/TalkPick_Server
 
-# 기존 컨테이너 제거
-if docker ps -a | grep -q "$CONTAINER_NAME"; then
-  docker stop "$CONTAINER_NAME"
-  docker rm "$CONTAINER_NAME"
-fi
+  # docker-compose 중지 및 컨테이너 제거
+  docker-compose down || true
 
-# 최신 이미지 pull
-docker pull "$IMAGE_URI"
-
-# 컨테이너 실행
-docker run -d \
-  --name "$CONTAINER_NAME" \
-  -p 8080:8080 \
-  "$IMAGE_URI"
+  # docker-compose 컨테이너 실행
+  docker-compose up -d
 
 echo "--------------- SUCCESS : Talkpick Server Deploy -----------------"
