da43e964a69306cdc30af0dfa1fd85b8f9376cd4
Hszoo <trre1827151@gmail.com>
2025-07-19 21:47:26 +0900

    [DEPLOY] start-server.sh 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,9 +6,9 @@ echo "--------------- START : Talkpick Server Deploy -----------------"
   cd /home/ubuntu/TalkPick_Server
 
   # docker-compose 중지 및 컨테이너 제거
-  docker-compose down || true
+  docker compose down || true
 
   # docker-compose 컨테이너 실행
-  docker-compose up -d
+  docker compose up -d
 
 echo "--------------- SUCCESS : Talkpick Server Deploy -----------------"
