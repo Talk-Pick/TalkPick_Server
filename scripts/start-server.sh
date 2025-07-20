b72cd1a12e0b79a102ee37f710b03794a950add3
Hszoo <trre1827151@gmail.com>
2025-07-20 11:04:47 +0900

    [DEPLOY] 압축 파일 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -8,20 +8,14 @@ echo "📂 현재 디렉토리: $(pwd)"
 echo "📄 파일 목록:"
 ls -al
 
-# ✅ .env 존재 확인
-if [ ! -f .env ]; then
-  echo "❌ .env 파일이 존재하지 않습니다. 배포 중단."
-  exit 1
-fi
-
 echo "🔐 ECR 로그인"
 aws ecr get-login-password --region ap-northeast-2 \
   | docker login --username AWS --password-stdin 718513646976.dkr.ecr.ap-northeast-2.amazonaws.com
 
-echo "🧹 기존 컨테이너 중지 및 정리"
-docker compose --env-file .env down || true
+echo "⏸️ 실행중인 컨테이너 중지"
+docker compose down || true
 
-echo "🚀 새 컨테이너 시작"
-docker compose --env-file .env up -d
+echo "▶️ 컨테이너 실행"
+docker compose up -d
 
 echo "✅ 서버 배포 완료"
