402590d1d26b287c2fd817c489f4633495256021
M-ung <wjdahrrla@gmail.com>
2025-08-17 14:50:39 +0900

    refactor: Define column constraints and add table descriptions to Search - #163

--- src/main/java/talkPick/domain/search/domain/TopicSearchHistory.java
+++ src/main/java/talkPick/domain/search/domain/TopicSearchHistory.java
@@ -1,9 +1,6 @@
 package talkPick.domain.search.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -12,13 +9,24 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "검색 기록 테이블")
 public class TopicSearchHistory {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, columnDefinition = "BIGINT COMMENT '기본 키'")
     private Long id;
+
+    @Column(name = "member_id", nullable = false, columnDefinition = "BIGINT COMMENT '회원 ID'")
     private Long memberId;
+
+    @Column(name = "word", nullable = false, length = 255, columnDefinition = "VARCHAR(255) COMMENT '검색 키워드'")
     private String word;
+
+    @Column(name = "search_at", nullable = false, columnDefinition = "DATETIME COMMENT '검색 시각'")
     private LocalDateTime searchAt;
+
+    @Column(name = "is_result_shown", nullable = false, columnDefinition = "TINYINT(1) COMMENT '검색 결과 표시 여부'")
     private Boolean isResultShown;
 
     public static TopicSearchHistory of(Long memberId, String word, Boolean isResultShown) {
