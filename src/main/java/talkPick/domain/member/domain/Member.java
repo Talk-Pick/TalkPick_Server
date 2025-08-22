a0de9f6d69c71194f314565616dcec95da798a31
simhyunmin <shm66371945@gmail.com>
2025-08-22 21:07:57 +0900

    refactor: Member 컬럼 definition 추가

--- src/main/java/talkPick/domain/member/domain/Member.java
+++ src/main/java/talkPick/domain/member/domain/Member.java
@@ -20,43 +20,84 @@ import java.time.LocalDate;
 public class Member extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column(
+            name = "member_id",
+            columnDefinition = "BIGINT COMMENT '회원 고유 PK'"
+    )
     private Long id;
 
-    @Column(length = 100, nullable = false)
+    @Column(
+            length = 100,
+            nullable = false,
+            columnDefinition = "VARCHAR(100) COMMENT '이메일(로그인 아이디)'"
+    )
     private String email;
 
-    @Column(length = 100)
+    @Column(
+            length = 100,
+            columnDefinition = "VARCHAR(100) COMMENT '비밀번호(암호화 저장)'"
+    )
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 6, nullable = false)
+    @Column(
+            length = 6,
+            nullable = false,
+            columnDefinition = "VARCHAR(6) COMMENT '회원 역할 (권한: ADMIN/USER 등)'"
+    )
     private Role memberRole;
 
-    @Column(length = 25, nullable = false)
+    @Column(
+            length = 25,
+            nullable = false,
+            columnDefinition = "VARCHAR(25) COMMENT '닉네임'"
+    )
     private String nickname;
 
+    @Column(
+            columnDefinition = "DATE COMMENT '생년월일'"
+    )
     private LocalDate birth;
 
     @Enumerated(EnumType.STRING)
+    @Column(
+            columnDefinition = "VARCHAR(10) COMMENT '성별(남/여 등)'"
+    )
     private Gender gender;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 6, nullable = false)
+    @Column(
+            length = 6,
+            nullable = false,
+            columnDefinition = "VARCHAR(6) COMMENT '로그인 타입(EMAIL, OAUTH 등)'"
+    )
     private LoginType loginType;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 10, nullable = false)
+    @Column(
+            length = 10,
+            nullable = false,
+            columnDefinition = "VARCHAR(10) COMMENT '회원 상태(활성, 휴면, 탈퇴 등)'"
+    )
     private TalkPickStatus status;
 
-
     @Enumerated(EnumType.STRING)
+    @Column(
+            columnDefinition = "VARCHAR(5) COMMENT 'MBTI 유형'"
+    )
     private MBTI mbti;
 
-    @Column(length = 255, nullable = false)
+    @Column(
+            length = 255,
+            nullable = false,
+            columnDefinition = "VARCHAR(255) COMMENT '프로필 이미지 URL'"
+    )
     private String profileImageUrl;
 
-    @Column(length = 255)
+    @Column(
+            length = 255,
+            columnDefinition = "VARCHAR(255) COMMENT 'OAuth Provider 식별값(구글, 카카오 등 연결용)'"
+    )
     private String providerId;
 
     public void updateNickname(String nickname) {
