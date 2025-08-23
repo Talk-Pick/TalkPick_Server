2ce2698b1dea5438fed24f18a735181dde0d0a32
M-ung <wjdahrrla@gmail.com>
2025-08-23 13:59:27 +0900

    refactor: Add annotation @Deprecated in unused code - #171

--- src/main/java/talkPick/domain/topic/exception/DuplicateLikeException.java
+++ src/main/java/talkPick/domain/topic/exception/DuplicateLikeException.java
@@ -3,6 +3,7 @@ package talkPick.domain.topic.exception;
 import talkPick.global.exception.ErrorCode;
 import talkPick.global.exception.TalkPickException;
 
+@Deprecated
 public class DuplicateLikeException extends TalkPickException {
     public DuplicateLikeException(final ErrorCode errorCode) {
         super(errorCode);
