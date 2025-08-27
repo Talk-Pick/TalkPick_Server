9ea004ae102359177ebeaf91baf9791a012733a1
M-ung <wjdahrrla@gmail.com>
2025-08-27 11:31:51 +0900

    feat: Add StatusCode in ApiResponse - #179

--- src/main/java/talkPick/global/response/ApiResponse.java
+++ src/main/java/talkPick/global/response/ApiResponse.java
@@ -19,56 +19,64 @@ public class ApiResponse<T> {
     private String message;
     private LocalDateTime timestamp;
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer httpStatus;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     public static <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
-                .status("success")
+                .status("SUCCESS")
                 .message("정상 처리되었습니다.")
                 .timestamp(LocalDateTime.now())
+                .httpStatus(200)
                 .data(data)
                 .build();
     }
 
     public static <T> ApiResponse<T> success(String message, T data) {
         return ApiResponse.<T>builder()
-                .status("success")
+                .status("SUCCESS")
                 .message(message)
                 .timestamp(LocalDateTime.now())
+                .httpStatus(200)
                 .data(data)
                 .build();
     }
 
     public static ApiResponse<Void> fail(String message) {
         return ApiResponse.<Void>builder()
-                .status("fail")
+                .status("FAIL")
                 .message(message)
                 .timestamp(LocalDateTime.now())
+                .httpStatus(400)
                 .build();
     }
 
     public static ApiResponse<Map<String, String>> fail(Map<String, String> errors) {
         return ApiResponse.<Map<String, String>>builder()
-                .status("fail")
+                .status("FAIL")
                 .message("입력값이 올바르지 않습니다.")
                 .timestamp(LocalDateTime.now())
+                .httpStatus(400)
                 .data(errors)
                 .build();
     }
 
     public static ApiResponse<Void> ofErrorCode(ErrorCode errorCode) {
         return ApiResponse.<Void>builder()
-                .status("fail")
+                .status("FAIL")
                 .message(errorCode.getMessage())
                 .timestamp(LocalDateTime.now())
+                .httpStatus(errorCode.getStatus().value())
                 .build();
     }
 
     public static <T> ApiResponse<T> ofErrorCode(ErrorCode errorCode, T data) {
         return ApiResponse.<T>builder()
-                .status("fail")
+                .status("FAIL")
                 .message(errorCode.getMessage())
                 .timestamp(LocalDateTime.now())
+                .httpStatus(errorCode.getStatus().value())
                 .data(data)
                 .build();
     }
