e5987a739d001fc14d45acf65932df9aad94dc9f
simhyunmin <shm66371945@gmail.com>
2025-09-01 18:52:22 +0900

    refactor: RedisCommandService에 Redis 저장되는 형태 고려해 로직 수정 - #186

--- src/main/java/talkPick/global/security/jwt/application/RedisCommandService.java
+++ src/main/java/talkPick/global/security/jwt/application/RedisCommandService.java
@@ -1,6 +1,7 @@
 package talkPick.global.security.jwt.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import talkPick.global.security.jwt.port.in.RedisCommandUseCase;
@@ -9,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class RedisCommandService implements RedisCommandUseCase {
     private final StringRedisTemplate redisTemplate;
     private static final String BLACKLIST_PREFIX = "blacklist:";
@@ -20,7 +22,12 @@ public class RedisCommandService implements RedisCommandUseCase {
 
     @Override
     public boolean isTokenBlacklisted(String token) {
-        return Boolean.TRUE.equals(redisTemplate.hasKey(BLACKLIST_PREFIX + token));
+        // 토큰에 Bearer가 없으면 자동 추가
+        if (!token.startsWith("Bearer ")) {
+            token = "Bearer " + token;
+        }
+        String key = BLACKLIST_PREFIX + token;
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
 
     @Override
