ea8133ae5e87c7c6886d403e1e8a43ce1f1e145a
simhyunmin <shm66371945@gmail.com>
2025-12-14 22:06:44 +0900

    refactor: 회원 컬럼 삭제로 TopicStat 또한 변경 - #250

--- src/main/java/talkPick/domain/random/dto/MemberDataDTO.java
+++ src/main/java/talkPick/domain/random/dto/MemberDataDTO.java
@@ -1,23 +1,12 @@
 package talkPick.domain.random.dto;
 
 import talkPick.domain.member.domain.Member;
-import talkPick.domain.member.domain.type.Gender;
 import talkPick.domain.member.domain.type.MBTI;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 public record MemberDataDTO(
-        MBTI mbti,
-        Gender gender,
-        Integer age
+        MBTI mbti
 ) {
     public static MemberDataDTO from(Member member) {
-        return new MemberDataDTO(member.getMbti(), member.getGender(), calculateAge(member.getBirth()));
-    }
-
-    private static int calculateAge(LocalDate birth) {
-        if (birth == null) return 0;
-        return Period.between(birth, LocalDate.now()).getYears();
+        return new MemberDataDTO(member.getMbti());
     }
 }
