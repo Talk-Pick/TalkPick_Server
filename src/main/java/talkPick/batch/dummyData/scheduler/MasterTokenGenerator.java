33ce4e4a2837ae519403b9a9e55e20d3fa9b7a2b
Zetty <wjdahrrla@gmail.com>
2026-01-27 10:26:00 +0900

    refactor: Local 일 때만 마스터 토큰 생성되도록 수정

--- src/main/java/talkPick/batch/dummyData/scheduler/MasterTokenGenerator.java
+++ src/main/java/talkPick/batch/dummyData/scheduler/MasterTokenGenerator.java
@@ -3,6 +3,7 @@ package talkPick.batch.dummyData.scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import talkPick.global.security.jwt.dto.JwtResDTO;
@@ -10,19 +11,13 @@ import talkPick.global.security.jwt.util.JwtGenerator;
 
 @Slf4j
 @Component
+@Profile("local")
 @RequiredArgsConstructor
 public class MasterTokenGenerator {
     private final JwtGenerator jwtGenerator;
 
     @EventListener(ApplicationReadyEvent.class)
     public void generateMasterToken() {
-        log.debug("\n");
-        log.debug("마스터 토큰 생성");
-        log.debug("========================================");
-
-        JwtResDTO.AccessToken masterToken = jwtGenerator.generateMasterAccessToken(1L, "MEMBER");
-
-        log.debug("{}", masterToken.accessToken());
-        log.debug("========================================\n");
+        log.info("[MasterToken Generated] AccessToken: {}", jwtGenerator.generateMasterAccessToken(1L, "MEMBER"));
     }
 }
